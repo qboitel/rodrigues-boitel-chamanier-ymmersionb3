9dbea1573abe547f00054f8367b89b851257f0b3
Quentin Boitel <quentin.boitel@outlook.fr>
2023-10-06 16:04:14 +0200

    feat: ajout select categorie

--- frontend/src/components/AddProduct.jsx
+++ frontend/src/components/AddProduct.jsx
@@ -1,6 +1,14 @@
+import {Fragment} from "react";
+import {Listbox, Transition} from "@headlessui/react";
+import {CheckIcon, ChevronUpDownIcon} from "@heroicons/react/24/solid/index.js";
+
 const AllSize = ["XS", "S", "M", "L", "XL", "XXL", "XXXL"]
 
 const AllColor = ["rouge", "vert", "bleu", "noir", "blanc", "jaune", "rose"]
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
 
 export default function AddProduct() {
 
@@ -53,6 +61,62 @@ export default function AddProduct() {
                     <label htmlFor="name">Catégorie du produit</label>
                     <input type="text" className="mt-2 block w-full rounded-lg border-0 bg-white py-1.5 px-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-red-600 sm:text-sm sm:leading-6" name="category" id="category" placeholder="Catégorie du produit" />
                 </div>
+                {/*<Listbox value={selected} onChange={setSelected}>
+                    {({ open }) => (
+                        <>
+                            <Listbox.Label className="block text-sm font-medium leading-6 text-gray-900">Catégorie</Listbox.Label>
+                            <div className="relative mt-2">
+                                <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                    <span className="block truncate">{selected.name}</span>
+                                    <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                                        <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                                    </span>
+                                </Listbox.Button>
+
+                                <Transition
+                                    show={open}
+                                    as={Fragment}
+                                    leave="transition ease-in duration-100"
+                                    leaveFrom="opacity-100"
+                                    leaveTo="opacity-0"
+                                >
+                                    <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                                        {people.map((person) => (
+                                            <Listbox.Option
+                                                key={person.id}
+                                                className={({ active }) =>
+                                                    classNames(
+                                                        active ? 'bg-indigo-600 text-white' : 'text-gray-900',
+                                                        'relative cursor-default select-none py-2 pl-3 pr-9'
+                                                    )
+                                                }
+                                                value={person}
+                                            >
+                                                {({ selected, active }) => (
+                                                    <>
+                                                        <span className={classNames(selected ? 'font-semibold' : 'font-normal', 'block truncate')}>
+                                                          {person.name}
+                                                        </span>
+                                                        {selected ? (
+                                                            <span
+                                                                className={classNames(
+                                                                    active ? 'text-white' : 'text-indigo-600',
+                                                                    'absolute inset-y-0 right-0 flex items-center pr-4'
+                                                                )}
+                                                            >
+                                                                <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                                            </span>
+                                                        ) : null}
+                                                    </>
+                                                )}
+                                            </Listbox.Option>
+                                        ))}
+                                    </Listbox.Options>
+                                </Transition>
+                            </div>
+                        </>
+                    )}
+                </Listbox>*/}
                 <div className="flex flex-col">
                     <label htmlFor="image">Image du produit</label>
                     <input type="file" className="mt-2 block w-full rounded-lg border-0 bg-white py-1.5 px-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-red-600 sm:text-sm sm:leading-6" id="pics" name="pics" multiple placeholder="Image du produit" />
